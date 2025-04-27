47e43128fd97770dd47216e0911a70909dd6fd10
bystrzejewskipawel <bystrzejewski.pawel@gmail.com>
2025-04-27 22:44:42 +0200

    CH2_L8

--- main.py
+++ main.py
@@ -3,26 +3,35 @@
 # throughout this file
 import pygame
 from constants import *
-import player
+from player import Player
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    p1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        updatable.update(dt)
+
         screen.fill("black")
-        p1.draw(screen)
-        p1.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
