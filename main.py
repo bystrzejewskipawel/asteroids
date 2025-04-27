e9d16104a0b7ac83ea906a71269faf79002b0458
bystrzejewskipawel <bystrzejewski.pawel@gmail.com>
2025-04-27 23:07:32 +0200

    CH3_L1

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -12,10 +14,14 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     
     while True:
 
@@ -31,7 +37,7 @@ def main():
             obj.draw(screen)
 
         pygame.display.flip()
-        
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
