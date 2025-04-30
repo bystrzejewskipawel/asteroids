42d2fa1669c5cb3dfbd59848da61f7ca82e1b8c5
bystrzejewskipawel <bystrzejewski.pawel@gmail.com>
2025-04-30 22:05:23 +0200

    CH3_L6

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
             for shot in shots:
                 if shot.collides_with(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
